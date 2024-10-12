c69472a3b618c99c9e960573e685dcac369f411a
Jérôme Nadaud <jerome@nadaud.io>
2024-10-12 10:33:57 +0200

    fix(docker): add no cache build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker buildx build --platform linux/amd64,linux/arm64 -t n3rv4/php-apache:latest --push .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64 -t n3rv4/php-apache:latest --push .
